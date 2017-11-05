73fdc8e66c23e999f68ea28f87e02b5e88559970
QueraltSM <queraltsosa@hotmail.com>
2017-11-05 09:20:42 +0000

    Update CalculadorEstadisticas.java

--- src/practica/pkg1/java/language/group/CalculadorEstadisticas.java
+++ src/practica/pkg1/java/language/group/CalculadorEstadisticas.java
@@ -1,28 +1,32 @@
 package practica.pkg1.java.language.group;
 public class CalculadorEstadisticas {
-    public static DatosEstadisticos[] estadisticaOrdenaVector(Ordenar1Vector ordenaVector, int size) {
-        DatosEstadisticos datosEstadistico = new DatosEstadisticos();
-        double contador = 0;
+    public static DatosEstadisticos[] estadisticaOrdenaVector(OrdenarVector m, int tam_vector){
+        DatosEstadisticos[] resultados = new DatosEstadisticos[2];
+        
+        // Modo aleatorio
+        resultados[1] = new DatosEstadisticos();
         for (int i = 0; i < 10; i++) {
-            ordenaVector.ordena(GeneraCaso.generaVector(size, true), datosEstadistico);
-            contador += datosEstadistico.dameTiempo();
+            int[] v = GeneraCaso.generaVector(tam_vector, true);
+            m.ordena(v, resultados[1]);
+            CompruebaCaso.compruebaVector(v, tam_vector);
         }
-
-        datosEstadistico.añadeTiempo(contador / 10);
-        DatosEstadisticos datosEstadisticosImpar = new DatosEstadisticos();
-        ordenaVector.ordena(GeneraCaso.generaVector(size, false), datosEstadisticosImpar);
-        return new DatosEstadisticos[]{datosEstadisticosImpar, datosEstadistico};
+       
+        resultados[1].estableceTiempo(resultados[1].dameTiempo()/10.0);
+        
+        // Modo inverso
+        resultados[0] = new DatosEstadisticos();
+        int[] v = GeneraCaso.generaVector(tam_vector, false);
+        m.ordena(v,resultados[0]);
+        CompruebaCaso.compruebaVector(v, tam_vector);
+            
+        return resultados;
     }
- //calcula los datos estadísticos para una secuencia de vectores de distintos tamaños 
-    public static DatosEstadisticos[][] estadisticasOrdenaVectores(OrdenarVector m, int[] tam_vectores) {
-        DatosEstadisticos[][] datosEstadisticos = new DatosEstadisticos[2][tam_vectores.length];
-        DatosEstadisticos[] tempCalculadorEstadistica;
+    
+    public static DatosEstadisticos[][] estadisticasOrdenaVectores(OrdenarVector m, int[] tam_vectores){
+        DatosEstadisticos[][] resultados = new DatosEstadisticos[tam_vectores.length][2];
         for (int i = 0; i < tam_vectores.length; i++) {
-            tempCalculadorEstadistica = estadisticaOrdenaVector(new Ordenar1Vector(), tam_vectores[i]);
-            datosEstadisticos[0][i] = tempCalculadorEstadistica[0];
-            datosEstadisticos[1][i] = tempCalculadorEstadistica[1];
-
+            resultados[i] = estadisticaOrdenaVector(m, tam_vectores[i]);
         }
-        return datosEstadisticos;
+        return resultados;
     }
 }
