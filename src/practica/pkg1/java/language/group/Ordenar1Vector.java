e747fcad231d6ff55e7f194d7abd8e342377de31
QueraltSM <queraltsosa@hotmail.com>
2017-11-05 09:20:08 +0000

    Update Ordenar1Vector.java

--- src/practica/pkg1/java/language/group/Ordenar1Vector.java
+++ src/practica/pkg1/java/language/group/Ordenar1Vector.java
@@ -1,26 +1,36 @@
 package practica.pkg1.java.language.group;
 public class Ordenar1Vector implements OrdenarVector{
+
     @Override
     public String nombreMetodo() {
-        return "Ordenar";
+        return "Intercambio de parejas";
     }
-
-    @Override 
+    
+    @Override
     public void ordena(int[] v, DatosEstadisticos de) {
-        int aux = 0;
-        de.estableceTiempo(0);
-        double tiempo = (double) System.currentTimeMillis();
-        for (int i = 0; i < v.length; i++) {
-            for (int j = 0; j < v.length; j++) {
-                if (v[i] < v[j]) {
-                    aux = v[i];
-                    v[i] = v[j];
-                    v[j] = aux;
+        boolean cambio;
+        int aux;
+        
+        double time = System.currentTimeMillis();
+        
+        for (int j = 0; j < v.length; j++){
+            cambio = false;
+            for (int i = (j+1)%2; i < v.length-1; i+=2){
+                if (v[i] > v[i+1]){
+                    aux = v[i+1];
+                    v[i+1] = v[i];
+                    v[i] = aux;
+                    cambio = true;
                 }
             }
-
+            if(!cambio && j>0){
+                time = System.currentTimeMillis() - time;
+                de.estableceTiempo(time);
+                return;
+            }
         }
-        double temp = System.currentTimeMillis() - tiempo;
-        de.añadeTiempo(temp);
+        time = System.currentTimeMillis() - time;
+        de.estableceTiempo(time);
+        return;
     }
 }
